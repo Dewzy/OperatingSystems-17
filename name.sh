76cf3b21be8113f91ce08e2a08e975d00e6d549a
Dew <fdew102@studentubuntu14.ecc.iwcc.edu>
2017-11-09 16:15:34 -0600

    First bash scripts with variables.

---
+++ name.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="Jesus"
+color="Gold"
+echo "Hello, $name"
+echo " "
+read -p "What is your favorite color? "
+echo " "
+read -p "How old are you? " age
+echo " "
+echo "$name is $age , his favorite color is $color , and you are $1."
+echo " "
